4d3a90237c8738863214b9c80d7468de3659ec5a
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-06-01 20:32:12 +0300

    mongo

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func main(){
-	fmt.Println("Hello Word")
+	"github.com/akhil/ecommerce-yt/controllers"
+	"github.com/akhil/ecommerce-yt/database"
+	"github.com/akhil/ecommerce-yt/middleware"
+	"github.com/akhil/ecommerce-yt/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
 }
